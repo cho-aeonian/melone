0eac552a4faa955356292e9490aea015b2280fd1
cho-aeonian <choxaeonian@gmail.com>
2025-06-23 08:32:50 +0900

    멜론...

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Chart100 from "./pages/chart100";
 import NewReleases from "./pages/NewReleases";
-import MyPage from "./pages/myPage";
+import MyPage from "./pages/mypage";
 import "./App.css";
 
 
